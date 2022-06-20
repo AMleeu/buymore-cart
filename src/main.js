d22fa26c6b65e058fd0f613b00ee6f412a09ed4d
AM leeu <albert.m.leeu@gmail.com>
2022-06-20 06:25:24 -0800

    installed & setup vue-router and created basic routes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import routes from "./routes";
 
-Vue.config.productionTip = false
+import App from "./App.vue";
+
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
+
+// instantiate Vue router
+const router = new VueRouter({
+  routes,
+  mode: "history",
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
