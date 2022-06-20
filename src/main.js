18662e739738d55050a381f6071a2cd1efb16e3d
AM leeu <albert.m.leeu@gmail.com>
2022-06-20 15:25:37 -0800

    installed bootstrap-vue & bootstrap and updated global styling of application

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import routes from "./routes";
 import { store } from "./store/store";
-
+import "bootswatch/dist/united/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 import App from "./App.vue";
 
+// Make BootstrapVue available throughout project
+Vue.use(BootstrapVue);
+// Make BootstrapVue BootstrapVue icon components available throughout project
+Vue.use(IconsPlugin);
+// initialize VueRouter and tell Vue to use it
 Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
