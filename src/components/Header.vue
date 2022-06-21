1e92cac3308b2c1727bfb40e24b9b1ea041efb89
AM leeu <albert.m.leeu@gmail.com>
2022-06-20 23:07:13 -0800

    updated Header component markup & styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,29 @@
 <template>
-    <h1>THIS IS THE HEADER</h1>
+  <div>
+    <b-navbar toggleable="lg" type="light" variant="light">
+      <b-navbar-brand href="#">buy more</b-navbar-brand>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="#">
+            <b-icon icon="cart" font-scale="1.5"></b-icon>
+            &nbsp; | &nbsp;
+            <b-badge variant="primary" class="header-badge">0</b-badge>
+          </b-nav-item>
+        </b-navbar-nav>
+    </b-navbar>
+  </div>
 </template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+.navbar-brand{
+    font-family: 'Bebas Neue', cursive;
+    font-size: 1.75rem;
+    color: rgba(0, 0, 0, 0.5) !important;
+}
+.header-badge{
+    font-size:0.95rem;
+    margin-top: -5px !important;
+}
+</style>
