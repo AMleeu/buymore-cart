4e7e18a66b21cd23e5a67837c6bf042f7aae2983
AM leeu <albert.m.leeu@gmail.com>
2022-06-22 08:46:22 -0800

    updated Header to get from store getter

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,42 +2,47 @@
   <div>
     <b-navbar toggleable="lg" type="light" variant="light">
       <b-navbar-brand href="#" @click="goToHome">buy more</b-navbar-brand>
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item href="#" @click="goToCart">
-            <b-icon icon="cart" font-scale="1.5"></b-icon>
-            &nbsp; | &nbsp;
-            <b-badge variant="primary" class="header-badge">
-              0
-            </b-badge>
-          </b-nav-item>
-        </b-navbar-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#" @click="goToCart">
+          <b-icon icon="cart" font-scale="1.5"></b-icon>
+          &nbsp; | &nbsp;
+          <b-badge variant="primary" class="header-badge">
+            {{ cartItemsCount }}
+          </b-badge>
+        </b-nav-item>
+      </b-navbar-nav>
     </b-navbar>
   </div>
 </template>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
-.navbar-brand{
-    font-family: 'Bebas Neue', cursive;
-    font-size: 1.75rem;
-    color: rgba(0, 0, 0, 0.5) !important;
+@import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap");
+.navbar-brand {
+  font-family: "Bebas Neue", cursive;
+  font-size: 1.75rem;
+  color: rgba(0, 0, 0, 0.5) !important;
 }
-.header-badge{
-    font-size:0.95rem;
-    margin-top: -5px !important;
+.header-badge {
+  font-size: 0.95rem;
+  margin-top: -5px !important;
 }
 </style>
 
 <script>
 export default {
-  methods:{
-    goToCart(){
-      this.$router.push({path:"/cart"})
+  methods: {
+    goToCart() {
+      this.$router.push({ path: "/cart" });
     },
-    goToHome(){
-      this.$router.push({path:"/"})
-    }
-  }
-}
+    goToHome() {
+      this.$router.push({ path: "/" });
+    },
+  },
+  computed: {
+    cartItemsCount() {
+      return this.$store.getters.cartItemsCount;
+    },
+  },
+};
 </script>
