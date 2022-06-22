89b8d16efdc1e928296ffafbf9d60a56c42a8512
AM leeu <albert.m.leeu@gmail.com>
2022-06-21 18:08:25 -0800

    updated Footer styling

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="footer">
-    &copy; buymore 2022 | <span> from the tv show CHUCK </span>
+    &copy; buymore 2022
   </div>
 </template>
 
@@ -11,11 +11,12 @@
   width: 100%;
   height: 2.5rem;
   line-height: 2.5rem;
-  background-color: #007bff;
-  color: #f8f9fa;
+  background-color: #dfdfe0 ;
+  color: rgba(0, 0, 0, 0.61);
   text-align: center;
+  border-top: 1px solid rgba(0, 0, 0, 0.125);
 }
 #footer span {
-  color: #dedede;
+  color: #212121;
 }
 </style>
