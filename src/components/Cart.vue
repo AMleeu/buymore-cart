76eaa2a3ff4f2f5fa2702c3ef9b0dd1d9520cf2f
AM leeu <albert.m.leeu@gmail.com>
2022-06-23 05:32:54 -0800

    added CartEmpty & CartWithItems components to conditionally display based on when there's items in cart

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,56 +1,12 @@
 <template>
   <div class="page-container">
     <Header />
-    <div class="container page-content">
-      <div class="row mt-5">
-        <div class="col-12 col-md-8 offset-md-2">
-          <b-list-group>
-            <b-list-group-item
-              href="#"
-              class="flex-column align-items-start"
-              v-for="item in cartItems"
-              :key="item.id"
-            >
-              <div
-                class="d-flex w-100 justify-content-between align-items-center"
-              >
-                <small class="cart-item-name">{{ item.name }}</small>
-                <b-badge variant="primary" class="cart-item-badge">
-                  {{ item.quantity }}
-                </b-badge>
-                <small>
-                 R {{item.price | formatPrice}} 
-                </small>
-                <div>
-                  <b-img
-                    center
-                    thumbnail
-                    fluid
-                    :src="item.imgUrl"
-                    alt="Center image"
-                    rounded
-                    class="cart-item-image"
-                  ></b-img>
-                </div>
-                <b-button-group>
-                  <b-button variant="secondary" @click="addToCart(item)">
-                    <b-icon icon="plus" font-scale="1.5"></b-icon>
-                  </b-button>
-                  <b-button variant="light" @click="removeFromCart(item)">
-                    <b-icon icon="dash" font-scale="1.5"></b-icon>
-                  </b-button>
-                </b-button-group>
-              </div>
-            </b-list-group-item>
-          </b-list-group>
-        </div>
-      </div>
-      <div class="row mb-4 mt-3">
-        <div class="col-6 offset-6 col-md-4 offset-md-6 text-right">
-            <b-alert show variant="light">total is: <hr/> R {{total | formatPrice}}</b-alert>
-        </div>
-      </div>
-    </div>
+    <!--
+      conditionally show CartEmpty if cart is empty 
+      conditionally show CartWithItems if cart is has items 
+     -->
+    <CartEmpty v-show="cartItemsCount === 0" />
+    <CartWithItems v-show="cartItemsCount > 0" />
     <Footer />
   </div>
 </template>
@@ -59,59 +15,20 @@
 // import components
 import Header from "./Header.vue";
 import Footer from "./Footer.vue";
-// import filters
-import {formatPrice} from "../filters/filters"
+import CartEmpty from "./CartEmpty.vue"
+import CartWithItems from "./CartWithItems.vue"
 
 export default {
   components: {
+    CartEmpty,
+    CartWithItems,
     Header,
     Footer,
   },
-  computed: {
-    cartItems() {
-      return this.$store.getters.cartItems;
-    },
-    total(){
-        let total = this.$store.getters.cartItems.reduce((currentTotal, item)=>{
-            return currentTotal += item.price * item.quantity;
-        }, 0)
-
-        return total
+  computed:{
+    cartItemsCount() {
+      return this.$store.getters.cartItemsCount;
     }
-  },
-  methods: {
-    addToCart(item) {
-      this.$store.commit("addToCart", item);
-    },
-    removeFromCart(item) {
-      this.$store.commit("removeFromCart", item);
-    },
-  },
-  filters:{
-    formatPrice,
   }
-};
+}
 </script>
-<style scoped>
-.cart-item-image {
-  width: 49px !important;
-  height: 49px !important;
-}
-.cart-item-badge {
-  transform: scale(1.2);
-}
-
-
-small.cart-item-name {
-  font-weight: bold;
-  min-width: 135.17px !important;
-  text-align: left;
-}
-@media (min-width: 768px) {
-  small.cart-item-name {
-    font-weight: normal;
-    transform: scale(1.12);
-    min-width: 146.75px !important;
-  }
-}
-</style>
---
+++ src/components/CartEmpty.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container page-content">
+    <div class="row mt-5">
+      <div class="col-12 col-md-8 offset-md-2">
+        <b-jumbotron
+          bg-variant="light"
+          text-variant="black"
+          border-variant="light"
+        >
+          <template #header>Your Cart</template>
+
+          <template #lead>
+            it currently empty, go to homepage to browse items
+          </template>
+
+          <hr class="my-4" />
+
+          <p class="justift-content-center">
+            <b-breadcrumb>
+              <b-breadcrumb-item href="#" @click="goToHome">
+                <b-icon
+                  icon="house-fill"
+                  scale="1.25"
+                  shift-v="1.25"
+                  aria-hidden="true"
+                ></b-icon>
+                Home
+              </b-breadcrumb-item>
+              <b-breadcrumb-item active>Cart</b-breadcrumb-item>
+            </b-breadcrumb>
+          </p>
+        </b-jumbotron>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goToHome() {
+      this.$router.push({ path: "/" });
+    },
+  },
+};
+</script>
